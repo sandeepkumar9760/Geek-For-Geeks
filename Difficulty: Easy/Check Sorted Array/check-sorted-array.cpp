e6c32f474d75340975b3099e91d807ad716b79ca
Sandeep Kumar <sandeepkumar270724@gmail.com>
2026-08-11 23:44:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check Sorted Array/check-sorted-array.cpp
+++ Difficulty: Easy/Check Sorted Array/check-sorted-array.cpp
@@ -1,13 +1,17 @@
 class Solution {
   public:
+    bool solve(vector<int>&arr , int idx){
+        if(idx==arr.size()){
+            return true;
+        }
+        if(arr[idx]<arr[idx-1]){
+            return false;
+        }else{
+            return solve(arr,idx+1);
+        }
+    }
     bool isSorted(vector<int>& arr) {
         // code here
-        if(arr.size()==1) return true;
-        for(int i=1;i<arr.size();i++){
-            if(arr[i]<arr[i-1]){
-                return false;
-            }
-        }
-        return true;
+        return solve(arr,1);
     }
 };
